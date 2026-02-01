9ee8e09bf54600c232f62771a847047c0b3f51a8
vnarendra5a1 <naren.kumar3@gmail.com>
2026-02-01 20:34:57 +0530

    Updated code base

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,13 +1,5 @@
-import { initKafka } from 'kafka/kafkaTransport'
-import { startHttpConfigWatcher } from './communication/config'
-import { startResponseListener } from 'kafka/listener'
 
 export * from '@core/utils/Log'
 export * from '@core/communication'
 export * from '@core/workflow/index'
 
-export async function initPlatform() {
-    await startHttpConfigWatcher()
-    await initKafka()
-    await startResponseListener()
-}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-export * as Core from '@core/index' 
+export * as Core from '@core/index'
+
+import { startHttpConfigWatcher } from '@core/communication/config'
+import { initKafka } from 'kafka/kafkaTransport'
+import { startResponseListener } from 'kafka/listener'
+
+export async function initPlatform() {
+    await startHttpConfigWatcher()
+    await initKafka()
+    await startResponseListener()
+}
